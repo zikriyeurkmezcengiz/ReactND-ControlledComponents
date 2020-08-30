603a7d7d3cf163063ec893744257efd3d8e0dfec
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-08-30 14:38:12 +0300

    Solution is completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    userInput: "",
+  };
+  onUserInputChange = (value) => {
+    this.setState(() => ({
+      userInput: value,
+    }));
+  };
   render() {
+    const { userInput } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -11,9 +20,14 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input
+            type="text"
+            placeholder="Say Something"
+            value={userInput}
+            onChange={(event) => this.onUserInputChange(event.target.value)}
+          />
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{userInput}</p>
         </div>
       </div>
     );
